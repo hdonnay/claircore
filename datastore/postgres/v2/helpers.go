5aea3e988b5d359fa908fa8eff258f37270c4dd4
Hank Donnay <hdonnay@redhat.com>
2024-08-16 16:45:56 -0500

    postgres/v2: remove some helpers
    Signed-off-by: Hank Donnay <hdonnay@redhat.com>

--- datastore/postgres/v2/helpers.go
+++ datastore/postgres/v2/helpers.go
@@ -1,40 +1,10 @@
 package postgres
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"iter"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/quay/zlog"
 )
-
-type txStart interface {
-	BeginTx(context.Context, pgx.TxOptions) (pgx.Tx, error)
-}
-
-func beginTxFunc(ctx context.Context, c txStart, opts pgx.TxOptions, f func(context.Context, pgx.Tx) error) error {
-	tx, err := c.BeginTx(ctx, opts)
-	if err != nil {
-		return fmt.Errorf("postgres: UpdaterV1: unable to start transaction: %w", err)
-	}
-	defer func() {
-		switch err := tx.Rollback(ctx); {
-		case errors.Is(err, nil):
-		case errors.Is(err, pgx.ErrTxClosed):
-		default:
-			zlog.Warn(ctx).
-				Err(err).
-				Msg("error rolling back transaction")
-		}
-	}()
-
-	if err := f(ctx, tx); err != nil {
-		return err
-	}
-	return tx.Commit(ctx)
-}
 
 type copyAdapter[T any] struct {
 	next func() (T, error, bool)
