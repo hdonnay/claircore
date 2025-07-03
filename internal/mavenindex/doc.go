63c3700c542be5815b26569bec2435debe70507b
Hank Donnay <hdonnay@redhat.com>
2025-07-03 13:06:12 -0500

    fixup! mavenindex: maven repository index parsing package

--- internal/mavenindex/doc.go
+++ internal/mavenindex/doc.go
@@ -1,3 +1,7 @@
 // Package mavenindex implements reading of incremental and full exports of a
 // maven repository search index.
+//
+// This package was built from reading the [java reader source].
+//
+// [java reader source]: https://github.com/apache/maven-indexer/tree/3229abf2d2efef595aca0bb1af26fb36425eb6ba/indexer-reader/src/main/java/org/apache/maven/index/reader
 package mavenindex
